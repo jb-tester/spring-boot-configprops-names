2b36705e7c41bd7a13fdb917addc8ab0770e0524
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2026-05-20 12:06:08 +0200

    records minor

--- src/main/java/com/mytests/spring/springbootconfigpropsnames/PropsRecordUsingNameAnno.java
+++ src/main/java/com/mytests/spring/springbootconfigpropsnames/PropsRecordUsingNameAnno.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.Name;
 
 @ConfigurationProperties("my.props.record-named")
-public record PropsRecordUsingNameAnno(@Name("boo") String prop1) {
+public record PropsRecordUsingNameAnno(
+        @Name("boo") String prop1,
+        @Name("booo")  String prop2) {
 
 }
